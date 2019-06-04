259b572ed9b4634e63a5df00bec3958ec4c5ffe8
Jake ORiley <ti1795sv@gmail.com>
2019-06-04 23:57:35 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ def turn_count(board)
   counter = 0
   board.each do |turn|
     if turn == "X" || turn == "O"
-    counter += 1
+    counter +=1
   end
   return counter
 end
@@ -15,3 +15,4 @@ end
     else
       return "0"
   end
+end
